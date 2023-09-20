c30d17d9082e9d78cda5d9eef8dc4b85c1ca5f74
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2023-09-20 09:29:29 +0700

    Header

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { Row, Col } from "antd";
+import "./HeaderComponent.scss";
+import { AudioOutlined } from "@ant-design/icons";
+import { Input, Space } from "antd";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+
+const { Search } = Input;
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: "#1890ff",
+    }}
+  />
+);
+const onSearch = (value) => console.log(value);
 
 function HeaderComponent() {
-  return <div>HeaderComponent</div>;
+  return (
+    <div>
+      <div className="WrapperHeader">
+        <Row className="row">
+          <Col span={6} className="wrapperTextHeader">
+            VANDUC
+          </Col>
+          <Col span={12}>
+            <Search
+              placeholder="input search text"
+              allowClear
+              enterButton
+              size="large"
+              onSearch={onSearch}
+            />
+          </Col>
+          <Col span={6} className="UserInfo">
+            <div className="WrapperAccount">
+              <UserOutlined className="AccountIcon" />
+              <div className="AccountInfo">
+                <span>Đăng nhập/Đăng ký</span>
+                <span>
+                  Tài khoản <CaretDownOutlined />
+                </span>
+              </div>
+            </div>
+            <div className="WrapperCart">
+              <ShoppingCartOutlined className="CartIcon" />
+              <span className="CartText">Giỏ hàng </span>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderComponent;
