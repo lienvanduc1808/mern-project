196989b65032aa99efd36459df1b2421107d0c4e
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2023-09-20 08:27:14 +0700

    Initialize project using Create React App

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./slices/counterSlices";
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
